5801fa673785357ac1dd5030935ed52b33a3f19f
Mateusz Laska <103432222+mateo139@users.noreply.github.com>
2023-03-16 18:38:57 +0100

    Delete obliczanie wymiarow podkladki.py

--- parts_of_code_&_ideas/obliczanie wymiarow podkladki.py
+++
@@ -1,5 +0,0 @@
-#obliczyć wymiary podkładki na podstawie wymiarów podpory
-#zastosować iterację od wielkości minimalnej do satysfakcjonującego wyniku
-#a1, b1 min 50, sprawdzać skok co 5 mm
-
-# A, B 
